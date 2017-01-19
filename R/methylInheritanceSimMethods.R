b22878ce64e127f5cdca1bb99f5cc23acf63bacd
belleau <pascal_belleau@hotmail.com>
2017-01-19 15:26:40 -0500

    Add documentation

--- R/methylInheritanceSimMethods.R
+++ R/methylInheritanceSimMethods.R
@@ -2,41 +2,81 @@
 #'
 #' @description TODO
 #'
-#' @param pathOut
+#' @param pathOut the path where the the out put file is seved
 #'
-#' @param fileGen
+#' @param fileGen a string include each output file. Each output file are 
+#' composed with a type name (methylGR, methylObj, ...), _, fileGen (F1),
+#'  parameters of the simulation, .rds
 #'
-#' @param nbSynCHR
+#' @param nbSynCHR Number of distinct synthetics chromosomes generate
 #'
-#' @param methData
+#' @param methData A real dataset where the program sampling to create 
+#' the synthetic chromosome
 #'
-#' @param nbBlock
+#' @param nbBlock the number of blocks sample from methData genome to create
+#' the synthetic chromosome
 #'
-#' @param lBlock
+#' @param lBlock the number of the CpG in each block sampled
 #'
-#' @param nbC
+#' @param nbC the number of CTRL and case in the the simulation dataset. In 
+#' the simulation dataset the number of CTRL equal the number of Case. 
+#' The number of CTRL do not need to be equal to the number of Case in
+#' the real dataset
 #'
-#' @param nbGeneration
+#' @param nbGeneration The number of generation simulate
 #'
-#' @param vpDiff
-#'
-#' @param vpDiffsd
-#'
-#' @param vDiff
-#'
-#' @param vInheritance
+#' @param vpDiff The proportion of case simulate as differentially methylated 
+#' when the site is selected as differentially methylated (this like the penetrance)
+#' at each differentially methylated site a number is sampled from a normal
+#' distribution with a truncated normal (between 0 and 1) vpDiff and variance 
+#' vpDIff this number will be the penetrance at the site
 #' 
-#' @param propInherite
+#' @param vpDiffsd the variance of the parameter vpDIff
 #'
-#' @param rateDiff
+#' @param vDiff in the case of a differentially methylated site a case 
+#' differentially methylated the proportion of C/T follow a beta distribution 
+#' where the mean is shifted of vDiff from the CTRL distribution
 #'
-#' @param minRate
-#'
-#' @param propHetero
+#' @param vInheritance the proportion of case inherite the inherited sites.
 #' 
-#' @param nbCores
+#' @param propInherite proportion of differentially methylated site
+#' are inherated
+#'
+#' @param rateDiff the mean of the chance that a site is differentially 
+#' methylated
+#'
+#' @param minRate the minimum number of diferentially methylated site
+#'
+#' @param propHetero the reduction vDiff for the intergeneration
 #' 
-#' @param keepDiff
+#' @param minReads a positive \code{integer} Bases and regions having lower
+#' coverage than this count are discarded. The parameter
+#' correspond to the \code{lo.count} parameter in the  \code{methylKit} package.
+#' 
+#' @param maxPercReads a \code{double} between [0,100], the percentile of read
+#' counts that is going to be used as upper cutoff. Bases ore regions
+#' having higher
+#' coverage than this percentile are discarded. Parameter used for both CpG
+#' sites and tiles analysis. The parameter
+#' correspond to the \code{hi.perc} parameter in the  \code{methylKit} package.
+#' 
+#' @param context Default: \code{"CpG"}
+#' 
+#' @param assembly Default: \code{"Rnor_5.0"}
+#' 
+#' @param meanCov Default: \code{80}
+#' 
+#' @param keepDiff Default: \code{FALSE}
+#' 
+#' @param saveGRanges Default: \code{TRUE}
+#' 
+#' @param saveMethylKit Default: \code{TRUE}
+#' 
+#' @param anaMethylKit Default: \code{TRUE}
+#' 
+#' @param nbCores Default: \code{1}.
+#' 
+#' @param vSeed Default: \code{-1}.
 #' 
 #' @param vSeed a \code{integer}, a seed used when reproducible results are
 #' needed. When a value inferior or equal to zero is given, a random integer
@@ -56,11 +96,13 @@
 
 
 runSim <- function(pathOut, fileGen, nbSynCHR, methData, nbBlock, lBlock,
-                   nbC, nbGeneration, vpDiff, vpDiffsd, vDiff, vInheritance,
-                   propInherite ,rateDiff, minRate, propHetero, minReads = 10, 
-                   maxPercReads = 99.9, context = "CpG", assembly="Rnor_5.0",
-                   meanCov = 80, n, 
-                   nbCores = 1, keepDiff = FALSE, vSeed = -1){
+                    nbC, nbGeneration, vpDiff, vpDiffsd, vDiff, vInheritance,
+                    propInherite ,rateDiff, minRate, propHetero, minReads = 10, 
+                    maxPercReads = 99.9, context = "CpG", assembly="Rnor_5.0",
+                    meanCov = 80, n, keepDiff = FALSE,
+                    saveGRanges = TRUE, saveMethylKit = TRUE,
+                    anaMethylKit = TRUE,
+                    nbCores = 1, vSeed = -1){
     
     if (vSeed <= -1) {
         tSeed <- as.numeric(Sys.time())
