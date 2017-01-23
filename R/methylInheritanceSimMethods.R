e6b2b84f1b1e3564724f404dfd3866e67e27321d
adeschen <adeschen@hotmail.com>
2017-01-22 22:01:02 -0500

    Adding validateRunSimParameters() function

--- R/methylInheritanceSimMethods.R
+++ R/methylInheritanceSimMethods.R
@@ -2,20 +2,22 @@
 #'
 #' @description TODO
 #'
-#' @param pathOut the path where the the output file is seved
+#' @param pathOut a string of \code{character}, the path where the 
+#' files created by the function will be saved.
 #'
-#' @param fileGen a \code{string} include each output file. Each output 
+#' @param fileGen a string of \code{character}, TODO 
+#' include each output file. Each output 
 #' file are 
 #' composed with a type name (methylGR, methylObj, ...), _, fileGen (ex F1),
 #' parameters of the simulation and ".rds". 
 #'
-#' @param nbSynCHR a positive \code{integer}, the number of distinct synthetics 
-#' chromosomes. generate.
+#' @param nbSynCHR a positive \code{integer}, the number of distinct synthetic 
+#' chromosomes. generate. TODO
 #'
-#' @param methData is object of class \code{methylBase}, the CpG information
+#' @param methData an object of class \code{methylBase}, the CpG information
 #' from controls (CTRL) that will be used to create the sythetic chromosome. 
-#' The object can also contain information from cases but only the controls will
-#' be used.
+#' The \code{methData} object can also contain information from cases but 
+#' only the controls will be used.
 #'
 #' @param nbBlock \code{integer}, the number of blocks used for sampling.
 #'
@@ -23,12 +25,12 @@
 #' for sampling from \code{methInfo}.
 #'
 #' @param vNbSample a \code{vector} of positive \code{integer}, the number of 
-#' CTRL and case in the the simulation dataset. In 
-#' the simulation dataset the number of CTRL equal the number of Case. 
+#' methData (CTRL) and cases in the the simulation dataset. In 
+#' the simulated dataset, the number of CTRL equals the number of Case. 
 #' The number of CTRL do not need to be equal to the number of Case in
-#' the real dataset
+#' the real dataset.
 #'
-#' @param nbGeneration a positive \code{integer}, the number of generations
+#' @param nbGeneration a positive \code{integer}, the number of generations.
 #'
 #' @param vpDiff a \code{double} superior to \code{0} and inferior or equal 
 #' to \code{1}, the mean value for the proportion of samples that will have,
@@ -51,13 +53,13 @@
 #'
 #' @param rateDiff a positive \code{double} inferior to \code{1}, the mean of 
 #' the chance that a site is differentially 
-#' methylated
+#' methylated.
 #'
 #' @param minRate a positive \code{double} inferior to \code{1}, the minimum 
-#' rate of differentially methylated sites
+#' rate of differentially methylated sites.
 #'
 #' @param propHetero a positive \code{double} between [0,1], the 
-#' reduction of vDiff for the second and following generation
+#' reduction of vDiff for the second and following generations.
 #' 
 #' @param minReads a positive \code{integer} Bases and regions having lower
 #' coverage than this count are discarded. The parameter
@@ -70,31 +72,34 @@
 #' sites and tiles analysis. The parameter
 #' correspond to the \code{hi.perc} parameter in the  \code{methylKit} package.
 #' 
-#' @param context methylation context string, ex: CpG,CpH,CHH, etc. (default:CpG)
+#' @param context a string of \code{character}, the methylation context 
+#' string, ex: CpG,CpH,CHH, etc. 
+#' Default: \code{"CpG"}.
 #' 
-#' @param assembly string that determines the genome assembly. Ex: mm9,hg18 etc.
+#' @param assembly a string of \code{character}, the short description of the 
+#' genome assembly. Ex: mm9,hg18 etc.
 #' Default: \code{"Rnor_5.0"}
 #' 
 #' @param meanCov a positive \code{integer} represent the mean of the coverage
-#' at the CpG site Default: \code{80}
+#' at the CpG site Default: \code{80}.
 #' 
-#' @param n a positive \code{integer} the number of simulation for each 
+#' @param n a positive \code{integer}, the number of simulation for each 
 #' parameters (\code{vNbSample}, \code{vpDiff}, \code{vDiff} and
-#' \code{vInheritance})
+#' \code{vInheritance}).
 #' 
 #' @param keepDiff \code{logical} if true, the differentially methyled sites
-#' will be the same for each parameters ( \code{vpDiff}, 
-#' \code{vDiff} and \code{vInheritance}). Default: \code{FALSE}
+#' will be the same for each parameter (\code{vpDiff}, 
+#' \code{vDiff} and \code{vInheritance}). Default: \code{FALSE}.
 #' 
 #' @param saveGRanges \code{logical} if true, save a list of \code{nbGeneration}
 #' GRangesList. Each GRangeList contain \code{vNbSample} GRanges which contain
 #' for each CpG site: the position, the coverage and the proportion of the C/T.
 #' A files treatment_... which contains the position of the CTRL and Case are 
-#' save too. Default: \code{TRUE}
+#' save too. Default: \code{TRUE}.
 #' 
-#' @param saveMethylKit Default: \code{TRUE}
+#' @param saveMethylKit a \code{logical}, TODO. Default: \code{TRUE}.
 #' 
-#' @param anaMethylKit Default: \code{TRUE}
+#' @param anaMethylKit a \code{logical}, TODO. Default: \code{TRUE}
 #' 
 #' @param nbCores a positive \code{integer}, the number of cores to use when
 #' processing the analysis. Default: \code{1}.
@@ -122,32 +127,53 @@
 #' @author Pascal Belleau
 #' @importFrom parallel mclapply
 #' @export
-
-
-
 runSim <- function(pathOut, fileGen, nbSynCHR, methData, nbBlock, lBlock,
-                   vNbSample, nbGeneration, vpDiff, vpDiffsd, vDiff, vInheritance,
-                    propInherite ,rateDiff, minRate, propHetero, minReads = 10, 
+                    vNbSample, nbGeneration, vpDiff, vpDiffsd, vDiff, 
+                    vInheritance,
+                    propInherite, rateDiff, minRate, propHetero, 
+                    minReads = 10, 
                     maxPercReads = 99.9, context = "CpG", assembly="Rnor_5.0",
                     meanCov = 80, n, keepDiff = FALSE,
                     saveGRanges = TRUE, saveMethylKit = TRUE,
                     anaMethylKit = TRUE,
-                    nbCores = 1, vSeed = -1){
+                    nbCores = 1, vSeed = -1) {
     
+    validateRunSimParameters(pathOut = pathOut, fileGen = fileGen, 
+                                nbSynCHR = nbSynCHR, methData = methData, 
+                                nbBlock = nbBlock, lBlock  = lBlock,
+                                vNbSample = vNbSample, 
+                                nbGeneration = nbGeneration, 
+                                vpDiff = vpDiff, vpDiffsd = vpDiffsd, 
+                                vDiff = vDiff, 
+                                vInheritance = vInheritance,
+                                propInherite = propInherite, 
+                                rateDiff, minRate, propHetero, 
+                                minReads = minReads, 
+                                maxPercReads = maxPercReads, 
+                                context = context, assembly = assembly,
+                                meanCov = meanCov, n, keepDiff = keepDiff,
+                                saveGRanges = saveGRanges, 
+                                saveMethylKit = saveMethylKit,
+                                anaMethylKit = anaMethylKit,
+                                nbCores = nbCores, vSeed = vSeed)
+    
+    ## Fix seed
     if (vSeed <= -1) {
         tSeed <- as.numeric(Sys.time())
         vSeed <- 1e8 * (tSeed - floor(tSeed))
     }
     set.seed(vSeed)
     
-    for(s in 1:nbSynCHR){
+    for(s in 1:nbSynCHR) {
         
-        # get the synthetic chr
-        res <- getSyntheticChr(methInfo=methData, nbBlock=nbBlock, nbCpG=lBlock)
+        # Create a synthetic chromosome
+        res <- getSyntheticChr(methInfo = methData, nbBlock = nbBlock, 
+                                nbCpG = lBlock)
+        
         adPref <- paste0(fileGen, "_", s)
-        saveRDS(res, file=paste0(pathOut, "/stateInfo_", adPref, ".rds"))
+        saveRDS(res, file = paste0(pathOut, "/stateInfo_", adPref, ".rds"))
         
-        for(nbSample in vNbSample){
+        for(nbSample in vNbSample) {
             
             adPrefSample <- paste0(adPref, "_", nbSample)
             nbCtrl <- nbSample
@@ -162,58 +188,68 @@ runSim <- function(pathOut, fileGen, nbSynCHR, methData, nbBlock, lBlock,
             
             sample.id <- list()
             
-            for(i in 1:(2*nbSample)){
-                if(i <= nbSample){
-                    for(j in 1:nbGeneration){
+            for (i in 1:(2*nbSample)) {
+                if (i <= nbSample) {
+                    for(j in 1:nbGeneration) {
                         if(i == 1){
                             sample.id[[j]] <- list()
                         }
                         sample.id[[j]][[i]] <- paste0("F", j,"_",i,"_C")
                     }
-                } else{
+                } else {
                     for(j in 1:nbGeneration){
                         sample.id[[j]][[i]] <- paste0("F", j,"_",i,"_OC")
                     }
                 }
             }
-            if(keepDiff == TRUE){
+            if (keepDiff == TRUE) {
                 diffRes <- getDiffMeth(stateInfo = res,
-                                       rateDiff = rateDiff, minRate = minRate,
-                                       propInherite = propInherite)
-            } else{
+                                        rateDiff = rateDiff, minRate = minRate,
+                                        propInherite = propInherite)
+            } else {
                 diffRes <- NULL
             }
-            for(i in 1:length(vInheritance)){
-                for(j in 1:length(vpDiff)){
+            
+            for (i in 1:length(vInheritance)) {
+                for(j in 1:length(vpDiff)) {
                     propDiff <- vpDiff[j]
                     propDiffsd <- vpDiffsd[j]
-                    for(k in 1:length(vDiff)){
+                    for (k in 1:length(vDiff)) {
                         diffValue <- vDiff[k]
-                        propInheritance <- ifelse(vInheritance[i]>=0, vInheritance[i], vpDiff[j])
-                        prefBase <- paste0(adPrefSample , "_", propDiff, "_", diffValue, "_", propInheritance)
+                        
+                        propInheritance <- ifelse(vInheritance[i] >= 0, 
+                                                vInheritance[i], vpDiff[j])
+                        
+                        prefBase <- paste0(adPrefSample , "_", propDiff, 
+                                            "_", diffValue, "_", 
+                                            propInheritance)
                             
                         a <- mclapply(1:n, FUN = simInheritance, 
-                                      pathOut = pathOut, 
-                                      pref = prefBase, nbCtrl = nbCtrl,
-                                      nbCase = nbCase, treatment = treatment, 
-                                      sample.id = sample.id, 
-                                      generation = nbGeneration,
-                                      stateInfo = res, rateDiff = rateDiff,
-                                      minRate = minRate, 
-                                      propInherite = propInherite,
-                                      diffValue = diffValue, 
-                                      propDiff = propDiff,
-                                      propDiffsd = propDiffsd, 
-                                      propInheritance = propInheritance,
-                                      propHetero = propHetero, 
-                                      minReads = minReads, 
-                                      maxPercReads = maxPercReads,
-                                      context = context, 
-                                      assembly = assembly,
-                                      meanCov = meanCov, diffRes = diffRes,
-                                      anaMethylKit = FALSE, 
-                                      mc.cores = nbCores,
-                                      mc.preschedule = FALSE)
+                                        pathOut = pathOut, 
+                                        pref = prefBase, 
+                                        nbCtrl = nbCtrl,
+                                        nbCase = nbCase, 
+                                        treatment = treatment, 
+                                        sample.id = sample.id, 
+                                        generation = nbGeneration,
+                                        stateInfo = res, 
+                                        rateDiff = rateDiff,
+                                        minRate = minRate, 
+                                        propInherite = propInherite,
+                                        diffValue = diffValue, 
+                                        propDiff = propDiff,
+                                        propDiffsd = propDiffsd, 
+                                        propInheritance = propInheritance,
+                                        propHetero = propHetero, 
+                                        minReads = minReads, 
+                                        maxPercReads = maxPercReads,
+                                        context = context, 
+                                        assembly = assembly,
+                                        meanCov = meanCov, 
+                                        diffRes = diffRes,
+                                        anaMethylKit = FALSE, 
+                                        mc.cores = nbCores,
+                                        mc.preschedule = FALSE)
                     }
                 }
             }
@@ -221,6 +257,3 @@ runSim <- function(pathOut, fileGen, nbSynCHR, methData, nbBlock, lBlock,
     }
     return(0)
 }
-
-
-
