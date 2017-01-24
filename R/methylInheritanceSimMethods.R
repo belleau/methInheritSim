fc448e58ff5440617918ccde0052dad0abb18c7a
belleau <pascal_belleau@hotmail.com>
2017-01-24 17:35:12 -0500

    Update man page and validateRunSimParameters function

--- R/methylInheritanceSimMethods.R
+++ R/methylInheritanceSimMethods.R
@@ -22,16 +22,15 @@
 #' \item the proportion of 
 #' C/T for a case differentially methylated follow a beta distribution, a
 #' number in the \code{vDiff} \code{vector}
-#' \item the proportion of 
-#' C/T for a case differentially methylated follow a beta distribution, a
-#' number in the \code{vDiff} \code{vector}
 #' \item the 
 #' proportion of cases that inherited differentially sites, a number in the
 #' \code{vInheritance} \code{vector}
-#' \item TODO
+#' \item Id for the simulation, a number 
+#' between 1 and \code{nbSimulation}
 #' \item the file extension ".rds"
 #' }
-#'
+#' Default: \code{"s"}.
+#' 
 #' @param nbSynCHR a positive \code{integer}, the number of distinct synthetic 
 #' chromosomes that will be generated. Default: \code{1}.
 #'
@@ -42,56 +41,75 @@
 #' 
 #' @param nbSimulation a positive \code{integer}, the number of simulations 
 #' for each parameter (\code{vNbSample}, \code{vpDiff}, \code{vDiff} and
-#' \code{vInheritance}).
+#' \code{vInheritance}). 
+#' The number total of simulation is 
+#' nbSimulation * \code{length(vNbSample)} * \code{length(vpDiff)} *
+#' \code{length(vInheritance)})
+#' Default: \code{10}
 #'
 #' @param nbBlock a positive \code{integer}, the number of blocks used 
 #' for sampling.
+#' Default: \code{100}
 #'
 #' @param nbCpG a positive \code{integer}, the number of consecutive CpG 
 #' positions used for sampling from \code{methInfo}.
+#' Default: \code{50}
 #'
-#' @param vNbSample a \code{vector} of positive \code{integer}, the number of 
+#' @param nbGeneration a positive \code{integer}, the number of generations.
+#' Default: \code{3}
+#'
+#' @param vNbSample a \code{vector} of distinct positive \code{integer}, the number of 
 #' controls (CTRL) and cases in the simulated dataset. In 
 #' the simulated dataset, the number of CTRL equals the number of cases. 
 #' The number of CTRL do not need to be equal to the number of Case in
 #' the real dataset \code{methData}.
+#' Default: \code{c(3, 6)}
 #'
-#' @param nbGeneration a positive \code{integer}, the number of generations.
-#'
-#' @param vpDiff a \code{vector} of \code{double} superior to \code{0} and 
-#' inferior or equal 
+#' @param vpDiff a \code{vector} of distinct \code{double} superior to \code{0} 
+#' and inferior or equal 
 #' to \code{1}, the mean value for the proportion of samples that will have,
 #' for a specific position, differentially methylated values. It can be 
-#' interpreted as the penetrance.
+#' interpreted as the penetrance. Note vpDiff and vpDiffsd must be
+#' the same length.
+#' Default: \code{c(0.9)}
 #' 
 #' @param vpDiffsd a \code{vector} of a non-negative \code{double}, the standard
-#' deviation associated to the \code{vpDiff}.
+#' deviation associated to the \code{vpDiff}. Note vpDiff and vpDiffsd must be
+#' the same length.
+#' Default: \code{c(0.1)}
 #'
-#' @param vDiff a \code{vector} of a positive \code{double} between [0,1], the proportion of 
+#' @param vDiff a \code{vector} of distinct non-negative \code{double} between [0,1], the proportion of 
 #' C/T for a case differentially methylated follow a beta distribution 
 #' where the mean is shifted of \code{vDiff} from the CTRL distribution
+#' Default: \code{c(0.8)}
 #'
-#' @param vInheritance a \code{vector} of positive \code{double} between [0,1], 
+#' @param vInheritance a \code{vector} of distinct non-negative \code{double} between [0,1], 
 #' the proportion of cases that inherited differentially sites.
+#' Default: \code{c(0.5)}
 #' 
-#' @param propInherite a non-negative \code{double} inferior or equal 
-#' to \code{1}, 
-#' proportion of differentially methylated site
-#' are inherated
-#'
 #' @param rateDiff a positive \code{double} inferior to \code{1}, the mean of 
 #' the chance that a site is differentially 
 #' methylated.
+#' Default: \code{0.01}
 #'
 #' @param minRate a non-negative \code{double} inferior to \code{1}, the 
 #' minimum rate for differentially methylated sites.
+#' Default: \code{0.01}
 #'
-#' @param propHetero a positive \code{double} between [0,1], the 
+#' @param propInherite a non-negative \code{double} inferior or equal 
+#' to \code{1}, 
+#' proportion of differentially methylated region
+#' are inherated
+#' Default: \code{0.3}
+#'
+#' @param propHetero a non-negative \code{double} between [0,1], the 
 #' reduction of vDiff for the second and following generations.
+#' Default: \code{0.5}
 #' 
 #' @param minReads a positive \code{integer} Bases and regions having lower
 #' coverage than this count are discarded. The parameter
 #' correspond to the \code{lo.count} parameter in the \code{methylKit} package.
+#' Default: \code{10}
 #' 
 #' @param maxPercReads a \code{double} between [0,100], the percentile of read
 #' counts that is going to be used as upper cutoff. Bases ore regions
@@ -99,25 +117,30 @@
 #' coverage than this percentile are discarded. Parameter used for both CpG
 #' sites and tiles analysis. The parameter
 #' correspond to the \code{hi.perc} parameter in the  \code{methylKit} package.
+#' Default: \code{99.9}
+#' 
+#' 
+#' @param meanCov a positive \code{integer}, the mean of the coverage
+#' at the CpG sites.
+#' Default: \code{80}
+#' 
 #' 
 #' @param context a string of \code{character}, the methylation context 
-#' string, ex: CpG,CpH,CHH, etc. 
+#'  one of the CpG,CpH,CHH, none. 
 #' Default: \code{"CpG"}.
 #' 
 #' @param assembly a string of \code{character}, the short description of the 
 #' genome assembly. Ex: "mm9", "hg18", etc.
 #' Default: \code{"Rnor_5.0"}
 #' 
-#' @param meanCov a positive \code{integer}, the mean of the coverage
-#' at the CpG sites. Default: \code{80}.
-#' 
 #' @param keepDiff a \code{logical}, when \code{TRUE}, the 
 #' differentially methyled sites
 #' will be the same for each parameter (\code{vpDiff}, 
-#' \code{vDiff} and \code{vInheritance}). Default: \code{FALSE}.
+#' \code{vDiff} and \code{vInheritance}).
+#' Default: \code{FALSE}
 #' 
 #' @param saveGRanges \code{logical} if true, save a list of \code{nbGeneration}
-#' GRangesList. Each GRangeList contain \code{vNbSample} GRanges which contain
+#' GRangesList. Each GRangeList contain \code{2 * vNbSample} GRanges which contain
 #' for each CpG site: the position, the coverage and the proportion of the C/T.
 #' A files treatment_... which contains the position of the CTRL and Case are 
 #' save too. Default: \code{TRUE}.
@@ -145,23 +168,26 @@
 #' \dontrun{runSim(outputDir = "testData", fileID = "F1", nbSynCHR = 1, 
 #' methData = samplesForChrSynthetic, nbSimulation = 2, 
 #' nbBlock = 10, nbCpG = 20,
-#' vNbSample = c(6), nbGeneration = 3, vpDiff = c(0.9), 
+#' nbGeneration = 3, vNbSample = c(6), vpDiff = c(0.9), 
 #' vpDiffsd = c(0.1), vDiff = c(0.8), 
 #' vInheritance = c(0.5), propInherite = 0.3,
-#' rateDiff = 0.3, minRate = 0.2, propHetero = 0.5, nbSimulation = 5, 
-#' nbCores= 1, vSeed = 32)}
+#' rateDiff = 0.3, minRate = 0.2, propHetero = 0.5, 
+#' nbCores = 1, vSeed = 32)}
 #' 
 #' @author Pascal Belleau
 #' @importFrom parallel mclapply
 #' @export
-runSim <- function(outputDir = NULL, fileID, nbSynCHR = 1, methData, 
-                    nbSimulation, nbBlock, nbCpG,
-                    vNbSample, nbGeneration, vpDiff, vpDiffsd, vDiff, 
-                    vInheritance,
-                    propInherite, rateDiff, minRate, propHetero, 
+runSim <- function(outputDir = NULL, fileID = "s", nbSynCHR = 1, methData, 
+                    nbSimulation = 10, nbBlock = 100, nbCpG = 50,
+                    nbGeneration = 3, vNbSample = c(3, 6), vpDiff = c(0.9), 
+                    vpDiffsd = c(0.1), vDiff = c(0.8), 
+                    vInheritance = c(0.5),
+                    rateDiff = 0.01, minRate = 0.01, propInherite = 0.3,
+                    propHetero = 0.5, 
                     minReads = 10, 
-                    maxPercReads = 99.9, context = "CpG", assembly="Rnor_5.0",
-                    meanCov = 80, keepDiff = FALSE,
+                    maxPercReads = 99.9, meanCov = 80,
+                    context = "CpG", assembly="Rnor_5.0",
+                    keepDiff = FALSE,
                     saveGRanges = TRUE, saveMethylKit = TRUE,
                     anaMethylKit = TRUE,
                     nbCores = 1, vSeed = -1) {
