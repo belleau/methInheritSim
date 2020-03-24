636a23bc06d9343db221ed70ac1994e9245bb41b
Astrid Deschenes <adeschen@hotmail.com>
2020-03-24 17:03:56 -0400

    Update unit tests to fix error caused by R update

--- inst/unitTests/test_methylInheritanceSimInternalMethods.R
+++ inst/unitTests/test_methylInheritanceSimInternalMethods.R
@@ -2122,49 +2122,49 @@ test.simEachGeneration_all_saveMethylKit_true <- function() {
                 saveGRanges = FALSE, saveMethylKit = TRUE, runAnalysis = FALSE)
     
     expGR_1 <- list()
-    expGR_1[[1]] <- new("methylRaw", data.frame(chr = rep("S", 3), 
+    expGR_1[[1]] <- new("methylRaw", data.frame(chr = factor(rep("S", 3)), 
                     start = c(1000, 1011, 1017), end = c(1000, 1011, 1017), 
                     strand = strand(rep("+", 3)), coverage = c(71, 90, 95), 
                     numCs = c(0, 1, 1), numTs = c(71, 89, 94)), sample.id = "F1_1_C", 
                     assembly = "RNOR_5.0", context = "Cpg", resolution = 'base')
-    expGR_1[[2]] <- new("methylRaw", data.frame(chr = rep("S", 3),  
+    expGR_1[[2]] <- new("methylRaw", data.frame(chr = factor(rep("S", 3)),  
                     start = c(1000, 1011, 1017), end = c(1000, 1011, 1017), 
                     strand = strand(rep("+", 3)), coverage = c(93, 92, 93), 
                     numCs = c(1, 4, 0), numTs = c(92, 88, 93)), sample.id = "F1_2_C", 
                     assembly = "RNOR_5.0", context = "Cpg", resolution = 'base')
-    expGR_1[[3]] <- new("methylRaw", data.frame(chr = rep("S", 3), 
+    expGR_1[[3]] <- new("methylRaw", data.frame(chr = factor(rep("S", 3)), 
                     start = c(1000, 1011, 1017), end = c(1000, 1011, 1017), 
                     strand = strand(rep("+", 3)), coverage = c(89, 87, 92), 
                     numCs = c(80, 2, 92), numTs = c(9, 85, 0)), sample.id = "F1_3_OC", 
                     assembly = "RNOR_5.0", context = "Cpg", resolution = 'base')
     expGR_2 <- list()
-    expGR_2[[1]] <- new("methylRaw", data.frame(chr = rep("S", 3), 
+    expGR_2[[1]] <- new("methylRaw", data.frame(chr = factor(rep("S", 3)), 
                         start = c(1000, 1011, 1017), end = c(1000, 1011, 1017), 
                         strand = strand(rep("+", 3)), coverage = c(90, 85, 79), 
                         numCs = c(0, 0, 1), numTs = c(90, 85, 78)), sample.id = "F2_1_C", 
                         assembly = "RNOR_5.0", context = "Cpg", resolution = 'base')
-    expGR_2[[2]] <- new("methylRaw", data.frame(chr = rep("S", 3),  
+    expGR_2[[2]] <- new("methylRaw", data.frame(chr = factor(rep("S", 3)),  
                         start = c(1000, 1011, 1017), end = c(1000, 1011, 1017), 
                         strand = strand(rep("+", 3)), coverage = c(73, 93, 78), 
                         numCs = c(0, 2, 0), numTs = c(73, 91, 78)), sample.id = "F2_2_C", 
                         assembly = "RNOR_5.0", context = "Cpg", resolution = 'base')
-    expGR_2[[3]] <- new("methylRaw", data.frame(chr = rep("S", 3), 
+    expGR_2[[3]] <- new("methylRaw", data.frame(chr = factor(rep("S", 3)), 
                         start = c(1000, 1011, 1017), end = c(1000, 1011, 1017), 
                         strand = strand(rep("+", 3)), coverage = c(83, 79, 78), 
                         numCs = c(83, 1, 0), numTs = c(0, 78, 78)), sample.id = "F2_3_OC", 
                         assembly = "RNOR_5.0", context = "Cpg", resolution = 'base')
     expGR_3 <- list()
-    expGR_3[[1]] <- new("methylRaw", data.frame(chr = rep("S", 3), 
+    expGR_3[[1]] <- new("methylRaw", data.frame(chr = factor(rep("S", 3)), 
                         start = c(1000, 1011, 1017), end = c(1000, 1011, 1017), 
                         strand = strand(rep("+", 3)), coverage = c(80, 73, 84), 
                         numCs = c(0, 0, 1), numTs = c(80, 73, 83)), sample.id = "F3_1_C", 
                         assembly = "RNOR_5.0", context = "Cpg", resolution = 'base')
-    expGR_3[[2]] <- new("methylRaw", data.frame(chr = rep("S", 3),  
+    expGR_3[[2]] <- new("methylRaw", data.frame(chr = factor(rep("S", 3)),  
                         start = c(1000, 1011, 1017), end = c(1000, 1011, 1017), 
                         strand = strand(rep("+", 3)), coverage = c(77, 80, 94), 
                         numCs = c(0, 2, 2), numTs = c(77, 78, 92)), sample.id = "F3_2_C", 
                         assembly = "RNOR_5.0", context = "Cpg", resolution = 'base')
-    expGR_3[[3]] <- new("methylRaw", data.frame(chr = rep("S", 3), 
+    expGR_3[[3]] <- new("methylRaw", data.frame(chr = factor(rep("S", 3)), 
                         start = c(1000, 1011, 1017), end = c(1000, 1011, 1017), 
                         strand = strand(rep("+", 3)), coverage = c(86, 79, 79), 
                         numCs = c(86, 2, 0), numTs = c(0, 77, 79)), sample.id = "F3_3_OC", 
@@ -2264,15 +2264,15 @@ test.simEachGeneration_all_runAnalysis_true <- function() {
     
     
     expDiff <- list()
-    expDiff[[1]] <- new("methylDiff", data.frame(chr = c("S"), start = c(1000), end = c(1000), 
+    expDiff[[1]] <- new("methylDiff", data.frame(chr = factor(c("S")), start = c(1000), end = c(1000), 
                         strand = strand(c("+")), pvalue=c(1.079053269533417e-72),
                         qvalue = c(0), meth.diff=c(63.066202090592341)), sample.ids = unlist(sampleID[[1]]), destranded = FALSE,
                         assembly = "RNOR_5.0", context = "Cpg", treatment = c(0,0,0,1,1,1), resolution = 'base')
-    expDiff[[2]] <- new("methylDiff", data.frame(chr = c("S"), start = c(1021), end = c(1021), 
+    expDiff[[2]] <- new("methylDiff", data.frame(chr = factor(c("S")), start = c(1021), end = c(1021), 
                         strand = strand(c("+")), pvalue=c(0.710492384389323),
                         qvalue = c(0), meth.diff=c(0.421067968237779)), sample.ids = unlist(sampleID[[2]]), destranded = FALSE,
                         assembly = "RNOR_5.0", context = "Cpg", treatment = c(0,0,0,1,1,1), resolution = 'base')
-    expDiff[[3]] <- new("methylDiff", data.frame(chr = c("S"), start = c(1037), end = c(1037), 
+    expDiff[[3]] <- new("methylDiff", data.frame(chr = factor(c("S")), start = c(1037), end = c(1037), 
                         strand = strand(c("+")), pvalue=c(2.227413948761407e-57),
                         qvalue = c(-3.681152020132998e-73), meth.diff=c(63.994307400379505)), sample.ids = unlist(sampleID[[3]]), destranded = FALSE,
                         assembly = "RNOR_5.0", context = "Cpg", treatment = c(0,0,0,1,1,1), resolution = 'base')
